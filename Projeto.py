a1d8844aab3d7d199189054c8d1acf24c9c18bfb
Gustavo de Moraes <gustavomoraesazevedo0@email.com>
2026-08-17 10:08:54 -0300

    Projeto de conversor foi finalizado e completado

--- Projeto.py
+++ Projeto.py
@@ -8,13 +8,25 @@ op = 0
 print("=-"*30)
 print("CONVERSOR DE DINHEIRO")
 moeda = float(input("digite um valor que deseja converter: R$"))
-print("[1] converter em bitcoin ₿")
-print("[2] conveter em dolar U$$")
-print("[3] converter em euros €")
-print("[4] digitar outro valor para converter R$")
-print("[5] sair do programa")
 while op != 5:
+  print("[1] converter em bitcoin ₿") 
+  print("[2] conveter em dolar U$$")
+  print("[3] converter em euros €")
+  print("[4] digitar outro valor para converter R$")
+  print("[5] sair do programa")
   op = int(input("sua opção :"))
-  if op == 1:
-    dolar = 5,21 * 1 
+  if op == 1: # converter para o bitcoin
+    bitcoin =  moeda * 239286.02
+    print("Com R${} é possivel converter {}₿".format(moeda,bitcoin))
+
+  if op == 2: # converter para o dolar
+    dolar = moeda * 0.19
     print("Com R${} é possivel converter {}U$$".format(moeda,dolar))
+
+  if op == 3: # converter para o Euro
+    euro = moeda * 0.17
+    print("Com R${} é possivel converter {}€".format(moeda,euro))
+
+  if op == 4: # adiciona novamente um valor para converter
+    print("Por favor informe um novo valor para ser convertido")
+    moeda = float(input("digite um valor que deseja converter R$"))
